dcc972748e685fef3d859639f14bede7e7ecbcc3
juliazib <julia.zibordi@gmail.com>
2024-10-05 22:35:03 -0300

    progresso na conversao de utf32 para utf8

--- T1/converteutf832.c
+++ T1/converteutf832.c
@@ -6,8 +6,10 @@ int convUtf8p32(FILE* arquivo_entrada, FILE* arquivo_saida);
 int verificaTamanhoCaractere(int chr);
 int* geraVetorDeBytes(int numBytes);
 int preencheVetorDeBytes(int* bytes, int numBytes, int cInicial, FILE* arquivo);
-int escreveCaractere(int* bytes, int numBytes, FILE* arquivo);
+int escreveCaractereUtf32(int* bytes, int numBytes, FILE* arquivo);
 int escreveBom(FILE* arquivo_saida);
+int eLittleEndian(unsigned char* bom);
+unsigned int pegaCodigo(unsigned char* chars, int littleEndian);
 
 int convUtf8p32(FILE* arquivo_entrada, FILE* arquivo_saida) {
     if (!escreveBom(arquivo_saida)) {
@@ -35,7 +37,7 @@ int convUtf8p32(FILE* arquivo_entrada, FILE* arquivo_saida) {
             return -1;
         }
 
-        int escrita = escreveCaractere(bytes, numBytesCaractere, arquivo_saida);
+        int escrita = escreveCaractereUtf32(bytes, numBytesCaractere, arquivo_saida);
         if (!escrita) {
             fprintf(stderr, "Erro na escrita do arquivo UTF-32\n");
             free(bytes); 
@@ -47,13 +49,46 @@ int convUtf8p32(FILE* arquivo_entrada, FILE* arquivo_saida) {
     return 1; 
 }
 
+int convUtf32p8(FILE* arquivo_entrada, FILE* arquivo_saida) {
+    unsigned char bom[4];
+    size_t bytes = fread(bom, sizeof(unsigned char), 4, arquivo_entrada);
+
+    if(bytes < 4 || eLittleEndian(bom) == -1) {
+        fprintf(stderr, "BOM inválido.\n");
+        return -1;
+    }
+
+    int littleEndian = eLittleEndian(bom) ? 1 : 0;
+
+    while(1) {
+        unsigned char chars[4];
+
+        size_t numChars = fread(chars, sizeof(unsigned char), 4, arquivo_entrada);
+
+        if(numChars < 4) {
+            if(feof(arquivo_entrada)) break;
+            else {
+                fprintf(stderr,"Erro na leitura do arquivo utf-32. \n");
+                return -1;
+            }
+        }
+
+        unsigned int codigo = pegaCodigo(chars, littleEndian);
+        
+    }
+
+    
+
+    return 0;
+}
+
 int escreveBom(FILE* arquivo_saida) {
     unsigned char bom[] = {0xFF, 0xFE, 0x00, 0x00};
     if (fwrite(bom, sizeof(unsigned char), 4, arquivo_saida) < 4) return 0;
     return 1;
 }
 
-int escreveCaractere(int* bytes, int numBytes, FILE* arquivo_saida) {
+int escreveCaractereUtf32(int* bytes, int numBytes, FILE* arquivo_saida) {
     unsigned int utf32chr = 0;
     int mask[] = {0x7F, 0x1F, 0x0F, 0x07};
 
@@ -93,4 +128,15 @@ int preencheVetorDeBytes(int* bytes, int numBytes, int cInicial, FILE* arquivo_e
 
 int* geraVetorDeBytes(int numBytes) {
     return (int*)malloc(sizeof(int) * numBytes);
+}
+
+int eLittleEndian(unsigned char* bom) {
+    if(bom[0] == 0x00 && bom[1] == 0x00 && bom[2] == 0xFE && bom[3] == 0xFF) return 0;
+    else if(bom[0] == 0xFF && bom[1] == 0xFE && bom[2] == 0x00 && bom[3] == 0x00) return 1;
+    else return -1; 
+}
+
+unsigned int pegaCodigo(unsigned char* chars, int littleEndian) {
+    if(!littleEndian) return chars[0] << 24 | chars[1] << 16 | chars[2] << 8 | chars[3];
+    else return chars[3] << 24 | chars[2] << 16 | chars[1] << 8 | chars[0];
 }
--- T1/main.c
+++ T1/main.c
@@ -3,8 +3,14 @@
 #include "converteutf832.h"
 
 int main(void){
-    FILE* entrada = fopen("utf8_peq.txt", "rb");
-    FILE* saida = fopen("testeConverte", "wb");
-    convUtf8p32(entrada, saida);
+    FILE* entrada_utf8 = fopen("utf8_peq.txt", "rb");
+    FILE* saida_utf32 = fopen("testeConverte", "wb");
+    convUtf8p32(entrada_utf8, saida_utf32);
+
+    FILE* entrada_utf32 = fopen("utf32_peq.txt", "rb");
+    FILE* saida_utf8 = fopen("testeConverte2", "wb");
+
+    convUtf32p8(entrada_utf32, saida_utf8);
+
     return 0;
 }
