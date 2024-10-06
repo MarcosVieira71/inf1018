5350a9ed97bb4d7a03341103d567f2cd877cb9bc
MarcosVieira71 <marcosvieira120404@gmail.com>
2024-10-06 16:55:48 -0300

    mudança de nome

--- T1/main.c
+++ T1/main.c
@@ -4,11 +4,11 @@
 
 int main(void){
     FILE* entrada_utf8 = fopen("utf8_peq.txt", "rb");
-    FILE* saida_utf32 = fopen("testeConverte", "wb");
+    FILE* saida_utf32 = fopen("utf32Novo.txt", "wb");
     convUtf8p32(entrada_utf8, saida_utf32);
 
     FILE* entrada_utf32 = fopen("utf32_peq.txt", "rb");
-    FILE* saida_utf8 = fopen("testeConverte2", "wb");
+    FILE* saida_utf8 = fopen("utf8Novo.txt", "wb");
 
     convUtf32p8(entrada_utf32, saida_utf8);
 
