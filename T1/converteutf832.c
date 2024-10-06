da33ed329c504e17cf3f511ad77ed4cab2c1b20f
MarcosVieira71 <marcosvieira120404@gmail.com>
2024-10-06 17:48:25 -0300

    mudanças no fluxo da funcao de escreve caractere utf8

--- T1/converteutf832.c
+++ T1/converteutf832.c
@@ -3,15 +3,15 @@
 #include "converteutf832.h"
 
 int convUtf8p32(FILE* arquivo_entrada, FILE* arquivo_saida);
-int verificaTamanhoCaractere(int chr);
-int* geraVetorDeBytes(int numBytes);
-int preencheVetorDeBytes(int* bytes, int numBytes, int cInicial, FILE* arquivo);
-int escreveCaractereUtf32(int* bytes, int numBytes, FILE* arquivo);
+int verificaTamanhoCaractereUTF8(int codigoDoCaractere);
+int* geraVetorDeBytes(int qtdBytes);
+int preencheVetorDeBytes(int* bytes, int qtdBytes, int cInicial, FILE* arquivo);
+int escreveCaractereUtf32(int* bytes, int qtdBytes, FILE* arquivo);
 int escreveBom(FILE* arquivo_saida);
 int eLittleEndian(unsigned char* bom);
 unsigned int pegaCodigo(unsigned char* chars, int littleEndian);
-int verificaNumeroBytes(unsigned int codigo);
-int escreveCaractereUtf8(unsigned int codigo, int qtdBytes, FILE* arquivo_saida);
+int verificaTamanhoCaractereUTF32(unsigned int codigoDoCarectere);
+int escreveCaractereUtf8(unsigned int codigoDoCarectere, int qtdBytes, FILE* arquivo_saida);
 
 int convUtf8p32(FILE* arquivo_entrada, FILE* arquivo_saida) {
     if (!escreveBom(arquivo_saida)) {
@@ -24,22 +24,22 @@ int convUtf8p32(FILE* arquivo_entrada, FILE* arquivo_saida) {
         cInicial = fgetc(arquivo_entrada);
         if (cInicial == EOF) break;
 
-        int numBytesCaractere = verificaTamanhoCaractere(cInicial);
-        int* bytes = geraVetorDeBytes(numBytesCaractere);
+        int qtdBytesCaractere = verificaTamanhoCaractereUTF8(cInicial);
+        int* bytes = geraVetorDeBytes(qtdBytesCaractere);
 
         if (!bytes) {
             fprintf(stderr, "Erro na alocação de memória\n");
             return -1;
         }
 
-        int leitura = preencheVetorDeBytes(bytes, numBytesCaractere, cInicial, arquivo_entrada);
+        int leitura = preencheVetorDeBytes(bytes, qtdBytesCaractere, cInicial, arquivo_entrada);
         if (!leitura) {
             fprintf(stderr, "Erro na leitura do arquivo UTF-8\n");
             free(bytes);  
             return -1;
         }
 
-        int escrita = escreveCaractereUtf32(bytes, numBytesCaractere, arquivo_saida);
+        int escrita = escreveCaractereUtf32(bytes, qtdBytesCaractere, arquivo_saida);
         if (!escrita) {
             fprintf(stderr, "Erro na escrita do arquivo UTF-32\n");
             free(bytes); 
@@ -53,11 +53,10 @@ int convUtf8p32(FILE* arquivo_entrada, FILE* arquivo_saida) {
 
 int convUtf32p8(FILE* arquivo_entrada, FILE* arquivo_saida) {
     unsigned char bom[4];
-    size_t bytes = fread(bom, sizeof(unsigned char), 4, arquivo_entrada);
+    fread(bom, sizeof(unsigned char), 4, arquivo_entrada);
+    int verificaLittleEndian = eLittleEndian(bom);
 
-    int littleEndian = eLittleEndian(bom);
-
-    if(bytes < 4 ||littleEndian == -1) {
+    if(verificaLittleEndian == -1) {
         fprintf(stderr, "BOM inválido.\n");
         return -1;
     }
@@ -75,11 +74,11 @@ int convUtf32p8(FILE* arquivo_entrada, FILE* arquivo_saida) {
             }
         }
 
-        unsigned int codigo = pegaCodigo(chars, littleEndian);
+        unsigned int codigoDoCarectere = pegaCodigo(chars, verificaLittleEndian);
 
-        int qtdBytes = verificaNumeroBytes(codigo);
+        int qtdBytes = verificaTamanhoCaractereUTF32(codigoDoCarectere);
 
-        if(!escreveCaractereUtf8(codigo, qtdBytes, arquivo_saida)) {
+        if(!escreveCaractereUtf8(codigoDoCarectere, qtdBytes, arquivo_saida)) {
             fprintf(stderr, "Erro na escrita do arquivo utf-8.\n");
             return -1;
         }
@@ -95,13 +94,13 @@ int escreveBom(FILE* arquivo_saida) {
     return 1;
 }
 
-int escreveCaractereUtf32(int* bytes, int numBytes, FILE* arquivo_saida) {
+int escreveCaractereUtf32(int* bytes, int qtdBytes, FILE* arquivo_saida) {
     unsigned int utf32chr = 0;
     int mask[] = {0x7F, 0x1F, 0x0F, 0x07};
 
-    utf32chr = bytes[0] & mask[numBytes - 1];
+    utf32chr = bytes[0] & mask[qtdBytes - 1];
 
-    for (int i = 1; i < numBytes; i++) {
+    for (int i = 1; i < qtdBytes; i++) {
         utf32chr = (utf32chr << 6) | (bytes[i] & 0x3F);
     }
 
@@ -109,22 +108,22 @@ int escreveCaractereUtf32(int* bytes, int numBytes, FILE* arquivo_saida) {
     return 1;
 }
 
-int verificaTamanhoCaractere(int chr) {
-    if (chr < 0x80) {
+int verificaTamanhoCaractereUTF8(int codigoDoCaractere) {
+    if (codigoDoCaractere < 0x80) {
         return 1;
-    } else if ((chr & 0xE0) == 0xC0) {
+    } else if ((codigoDoCaractere & 0xE0) == 0xC0) {
         return 2;
-    } else if ((chr & 0xF0) == 0xE0) {
+    } else if ((codigoDoCaractere & 0xF0) == 0xE0) {
         return 3;
-    } else if ((chr & 0xF8) == 0xF0) {
+    } else if ((codigoDoCaractere & 0xF8) == 0xF0) {
         return 4;
     }
     return 0;
 }
 
-int preencheVetorDeBytes(int* bytes, int numBytes, int cInicial, FILE* arquivo_entrada) {
+int preencheVetorDeBytes(int* bytes, int qtdBytes, int cInicial, FILE* arquivo_entrada) {
     bytes[0] = cInicial;
-    for (int i = 1; i < numBytes; i++) {
+    for (int i = 1; i < qtdBytes; i++) {
         int cTemp = fgetc(arquivo_entrada);
         if (cTemp == EOF) return 0;
 
@@ -133,8 +132,8 @@ int preencheVetorDeBytes(int* bytes, int numBytes, int cInicial, FILE* arquivo_e
     return 1;
 }
 
-int* geraVetorDeBytes(int numBytes) {
-    return (int*)malloc(sizeof(int) * numBytes);
+int* geraVetorDeBytes(int qtdBytes) {
+    return (int*)malloc(sizeof(int) * qtdBytes);
 }
 
 int eLittleEndian(unsigned char* bom) {
@@ -148,36 +147,28 @@ unsigned int pegaCodigo(unsigned char* chars, int littleEndian) {
     else return (unsigned int) chars[3] << 24 | chars[2] << 16 | chars[1] << 8 | chars[0];
 }
 
-int verificaNumeroBytes(unsigned int codigo) {
-    if(codigo > 0x10ffff) {
+int verificaTamanhoCaractereUTF32(unsigned int codigoDoCaractere) {
+    if(codigoDoCaractere > 0x10ffff) {
         fprintf(stderr, "Código Unicode inválido.\n");
         return 0;
     }
-    else if(codigo < 0x80) return 1;
-    else if(codigo < 0x800) return 2;
-    else if(codigo < 0x10000) return 3;
+    else if(codigoDoCaractere < 0x80) return 1;
+    else if(codigoDoCaractere < 0x800) return 2;
+    else if(codigoDoCaractere < 0x10000) return 3;
     else return 4;
 
 }
 
-int escreveCaractereUtf8(unsigned int codigo, int qtdBytes, FILE* arquivo_saida) {
-    if(qtdBytes == 1) {
-        unsigned char byte = (unsigned char) codigo;
-        if (fwrite(&byte, sizeof(unsigned char), 1, arquivo_saida) < 1) return 0;
+int escreveCaractereUtf8(unsigned int codigoDoCaractere, int qtdBytes, FILE* arquivo_saida) {
+    unsigned char bytes[4];
+    if (qtdBytes == 1) bytes[0] = (unsigned char) codigoDoCaractere;
+    else {
+        for(int i = qtdBytes-1; i > 0; i--){
+            bytes[i] = 0x80 | (codigoDoCaractere & 0x3f);
+            codigoDoCaractere >>= 6;
+        }
+        bytes[0] = (unsigned char)((0xff << (8 - qtdBytes)) | codigoDoCaractere);
     }
-    else if(qtdBytes == 2) {
-        unsigned char bytes[2] = { 0xc0 | (codigo >> 6), 0x80 | (codigo & 0x3f)};
-        if(fwrite(bytes, sizeof(unsigned char), 2, arquivo_saida) < 2) return 0;
-    }
-    else if(qtdBytes == 3) {
-        unsigned char bytes[3] = {0xe0 | (codigo >> 12), 0x80 | ((codigo >> 6) & 0x3f), 0x80 | (codigo & 0x3f)};
-        if(fwrite(bytes, sizeof(unsigned char), 3, arquivo_saida) < 3) return 0; 
-    }
-    else if(qtdBytes == 4) {
-        unsigned char bytes [4] = {0xf0 | (codigo >> 18), 0x80 | ((codigo >> 12) & 0x3f), 0x80 | ((codigo >> 6) & 0x3f), 0x80 | (codigo & 0x3f)};
-        if(fwrite(bytes, sizeof(unsigned char), 4, arquivo_saida) < 4) return 0;
-    }
-
+    if (fwrite(bytes, sizeof(unsigned char), qtdBytes, arquivo_saida) < qtdBytes) return 0;
     return 1;
-
 }
